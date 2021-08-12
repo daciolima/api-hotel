906c8ad09399810da8e59d326fcaf25712fc8702
Dácio Lima <daciolima@MacBook-Pro.local>
2021-08-12 19:47:43 -0300

    implementação do get do recurso Hotel

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from dynaconf import FlaskDynaconf
 from flask_restful import Api
 from resources.hotel import Hoteis
+from resources.hotel import Hotel
 
 app = Flask(__name__)
 FlaskDynaconf(app)
@@ -9,6 +10,7 @@ api = Api(app)
 
 
 api.add_resource(Hoteis, '/hoteis')
+api.add_resource(Hotel, '/hotel/<int:hotel_id>')
 
 
 if __name__ == '__main__':
--- resources/hotel.py
+++ resources/hotel.py
@@ -2,37 +2,37 @@ from flask_restful import Resource
 
 
 hoteis = [
-    {'hotel_id': 564,
+    {'hotel_id': 560,
      'name': 'Plaza Hotel',
      'star': 4.3,
      'diaria': 450.00,
      'cidade': 'João Pessoa-PB'
      },
-    {'hotel_id': 565,
+    {'hotel_id': 561,
      'name': 'Chica Hotel',
      'star': 4.0,
      'diaria': 420.00,
      'cidade': 'Fortaleza-CE'
      },
-    {'hotel_id': 566,
+    {'hotel_id': 562,
      'name': 'Drummont Hotel',
      'star': 4.7,
      'diaria': 400.00,
      'cidade': 'Rio de Janeiro-RJ'
      },
-    {'hotel_id': 567,
+    {'hotel_id': 563,
      'name': 'Praia Hotel',
      'star': 4.2,
      'diaria': 4220.00,
      'cidade': 'Recife-PE'
      },
-    {'hotel_id': 568,
+    {'hotel_id': 564,
      'name': 'Sol e Mar',
      'star': 4.3,
      'diaria': 400.00,
      'cidade': 'João Pessoa-PB'
      },
-    {'hotel_id': 569,
+    {'hotel_id': 565,
      'name': 'Sol Nascente',
      'star': 4.4,
      'diaria': 450.00,
@@ -44,3 +44,21 @@ hoteis = [
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
+
+
+class Hotel(Resource):
+
+    def get(self, hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+        return {'messagem': 'Hotel não encontrado'}, 404
+
+    def post(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
