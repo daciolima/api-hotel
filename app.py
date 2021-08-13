b628a25b194842fe651aa4f4de1de82987f68ed5
Dácio Lima <daciolima@MacBook-Pro.local>
2021-08-13 16:41:00 -0300

    code conf sqlalchemy e classe hotel

--- app.py
+++ app.py
@@ -3,10 +3,19 @@ from dynaconf import FlaskDynaconf
 from flask_restful import Api
 from resources.hotel import Hoteis
 from resources.hotel import Hotel
+from sql_alchemy import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db_hotel.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 FlaskDynaconf(app)
 api = Api(app)
+db.init_app(app)
+
+
+@app.before_first_request
+def cria_banco():
+    db.create_all()
 
 
 api.add_resource(Hoteis, '/hoteis')
--- models/hotel.py
+++ models/hotel.py
@@ -1,4 +1,15 @@
-class HotelModel:
+from sql_alchemy import db
+
+
+class HotelModel(db.Model):
+
+    __tablename__ = 'hoteis'
+    hotel_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    star = db.Column(db.Float(precision=1))
+    daily = db.Column(db.Float(precision=2))
+    city = db.Column(db.String(80))
+
     def __init__(self, hotel_id, name, star, diaria, cidade):
         self.hotel_id = hotel_id
         self.name = name
