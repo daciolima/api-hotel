511ec6e6a31e4cbb396ceeb98d14856cdb3ca555
Dácio Lima <daciolima@MacBook-Pro.local>
2021-08-19 15:59:47 -0300

    Implementação de filtros de busca

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,14 +1,79 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 from flask_jwt_extended import jwt_required
+import sqlite3
+
+
+def normalize_path_params(city=None,
+                          star_min=0,
+                          star_max=5,
+                          daily_min=0,
+                          daily_max=10000,
+                          limit=50,
+                          offset=0, **dados):
+    if city:
+        return {
+            'star_min': star_min,
+            'star_max': star_max,
+            'daily_min': daily_min,
+            'daily_max': daily_max,
+            'city': city,
+            'limit': limit,
+            'offset': offset
+        }
+
+    return {
+        'star_min': star_min,
+        'star_max': star_max,
+        'daily_min': daily_min,
+        'daily_max': daily_max,
+        'limit': limit,
+        'offset': offset
+    }
+
+
+# parametros para filtros personalizados
+path_params = reqparse.RequestParser()
+path_params.add_argument('city', type=str)
+path_params.add_argument('star_min', type=float)
+path_params.add_argument('star_max', type=float)
+path_params.add_argument('daily_min', type=float)
+path_params.add_argument('daily_max', type=float)
+path_params.add_argument('limit', type=float)
+path_params.add_argument('offset', type=float)
 
 
 class Hoteis(Resource):
     def get(self):
-        hoteis_encontrados = HotelModel.hotel_all()
-        if hoteis_encontrados:
-            return {"hoteis": [hotel.json() for hotel in hoteis_encontrados]}
-        return {"message": "Nenhum hotel cadastrado"}
+
+        connection = sqlite3.connect('db_hotel.db')
+        cursor = connection.cursor()
+
+        dados = path_params.parse_args()
+        dados_validos = {chave: dados[chave] for chave in dados if dados[chave] is not None}
+        parametros = normalize_path_params(**dados_validos)
+
+        if not parametros.get('city'):
+            consulta = "SELECT * FROM hoteis WHERE (star >= ? and star <= ?) and (daily >= ? and daily <= ?) LIMIT ? OFFSET ?"
+            tupla = tuple([parametros[chave] for chave in parametros])
+            resultado = cursor.execute(consulta, tupla)
+
+        else:
+            consulta = "SELECT * FROM hoteis WHERE (star >= ? and star <= ?) and (daily >= ? and daily <= ?) and city = ? LIMIT ? OFFSET ?"
+            tupla = tuple([parametros[chave] for chave in parametros])
+            resultado = cursor.execute(consulta, tupla)
+
+        hoteis = []
+        for linha in resultado:
+            hoteis.append({
+                'hotel_id': linha[0],
+                'nome': linha[1],
+                'star': linha[2],
+                'daily': linha[3],
+                'city': linha[4],
+            })
+
+        return {"hoteis": hoteis}
 
 
 class Hotel(Resource):
